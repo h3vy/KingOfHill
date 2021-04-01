b8ecaaf1c49ae6ad5a1bed99ef0c6c81d41bdc84
h3vy <Stepan1009@mail.ru>
2021-04-01 23:33:09 +0300

    init

--- client/src/KingOfHill/client/ConnectNigor.java
+++ client/src/KingOfHill/client/ConnectNigor.java
@@ -1,4 +1,15 @@
 package KingOfHill.client;
 
+import java.io.*;
+import java.net.Socket;
+
 public class ConnectNigor {
+
+    public static void main(String[] args) {
+        byte[] request = "1234567".getBytes();
+        ByteArrayOutputStream out=new ByteArrayOutputStream();
+        out.write(request);
+        System.out.println(request.toString());
+    }
+
 }
